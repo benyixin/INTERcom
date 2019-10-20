e22329b2349c2ff17d0083bf837197e7aba3be12
benyixin <15125297633@163.com>
2019-10-20 14:57:41 +0800

    start

---
+++ 实验一/实验作业一/Serverone/Serverone/Serverone.h
@@ -0,0 +1,32 @@
+
+// Serverone.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CServeroneApp: 
+// 有关此类的实现，请参阅 Serverone.cpp
+//
+
+class CServeroneApp : public CWinApp
+{
+public:
+	CServeroneApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CServeroneApp theApp;
